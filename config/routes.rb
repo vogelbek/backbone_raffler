ccce7f16746634a50505ddb345f785355eda020f
Adam Hendricksen <adam@nird.us>
2014-05-25 17:01:35 -0700

    Adding basic model support

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  scope 'api' do
+    resources :entries
+  end
+
   root to: 'main#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
